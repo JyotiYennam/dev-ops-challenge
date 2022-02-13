2466407732777f6c77c4167fdfd0a80238ec0ccc
Jyoti Yennam <jyoti@ellefsontech.com>
2022-02-13 14:28:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+#S3 bucket to store terraform state files
 terraform {
   backend "s3" {
     bucket         = "terrafrom-state.app.devopschallenge"
@@ -9,6 +10,7 @@ terraform {
 provider "aws" {
    region = "us-east-1"
  }
+
 
 #VPC
 resource "aws_vpc" "main" {
@@ -23,6 +25,7 @@ resource "aws_vpc" "main" {
   )
 }
 
+
 #Subnets
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
@@ -30,6 +33,7 @@ resource "aws_subnet" "main" {
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 }
+
 
 #Route tables and associations
 resource "aws_internet_gateway" "public" {
@@ -40,7 +44,6 @@ resource "aws_internet_gateway" "public" {
     }
   )
 }
-
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   tags = merge(
@@ -49,12 +52,10 @@ resource "aws_route_table" "public" {
     }
   )
 }
-
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.main.id
   route_table_id = aws_route_table.public.id
 }
-
 resource "aws_route" "public" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
@@ -63,3 +64,74 @@ resource "aws_route" "public" {
 }
 
 
+#RDS
+resource "aws_security_group" "rdssc" {
+  name   = "rds-securitygroup"
+  vpc_id = aws_vpc.main.id
+  description = "security group for rds"
+
+  tags = merge(
+    {
+      "Name" = "DB Security Group"
+    }
+  )
+}
+
+# Open ingress traffic SSH port
+resource "aws_security_group_rule" "open_ingress" {
+  depends_on        = [aws_security_group.rdssc]
+  type              = "ingress"
+  description       = "Open Input for 5432 "
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
+  cidr_blocks       = "10.1.0.0/16"
+  security_group_id = aws_security_group.rdssc.id
+}
+   
+# Open egress traffic
+resource "aws_security_group_rule" "open_egress" {
+  depends_on        = [aws_security_group.rdssc]
+  type              = "egress"
+  description       = "OPEN egress, all ports, all protocols"
+  from_port         = "0"
+  to_port           = "0"
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.rdssc.id
+}
+
+resource "aws_db_subnet_group" "db_subnet" {
+  name_prefix = "subnet-grp"
+  description = "Database subnet group for rds"
+  subnet_ids  = aws_subnet.main.id
+}
+
+resource "aws_db_instance" "default" {
+  identifier = "hello_world"
+  instance_class = ""db.t2.micro"
+  allocated_storage = 5
+  username = admin
+  password = admin
+  port = 5432
+  engine    = "postgres"
+  engine_version = "11.6"
+  storage_type  = "gp2"
+  iam_database_authentication_enabled = false
+  storage_encrypted = false
+  vpc_security_group_ids = [aws_security_group.rdssc.id]
+  db_subnet_group_name = aws_db_subnet_group.db_subnet.id
+  availability_zone   = "us-east-1"
+  multi_az            = false
+  iops                = 0
+  publicly_accessible = true
+  allow_major_version_upgrade = false
+  auto_minor_version_upgrade  = true
+  apply_immediately           = true
+  maintenance_window          = "Mon:00:00-Mon:03:00"
+  deletion_protection         = false
+  copy_tags_to_snapshot       = true
+  backup_retention_period = 30
+  backup_window           = "03:00-06:00"
+  enabled_cloudwatch_logs_exports = ["error", "general", "slowquery"]
+}
